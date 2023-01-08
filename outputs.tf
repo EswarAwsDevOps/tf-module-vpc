959ab40d4708938381461a6765d3cf310f958721
eswar <eswar.awsdevops@gmail.com>
2023-01-08 17:01:53 +0530

    creating mutable app

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,10 @@ output "app_subnets_ids" {
 output "app_subnets_cidr" {
   value = aws_subnet.app_subnets.*.cidr_block
 }
+output "db_subnets_ids" {
+  value = aws_subnet.db_subnets.*.id
+}
+
+output "db_subnets_cidr" {
+  value = aws_subnet.db_subnets.*.cidr_block
+}
