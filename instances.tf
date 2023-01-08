d00c9547d64be49690906835988382a10ff5da9f
eswar <eswar.awsdevops@gmail.com>
2023-01-08 19:20:49 +0530

    creating mutable app

--- instances.tf
+++ instances.tf
@@ -1,21 +1,21 @@
-resource "aws_instance" "public" {
-  ami = "ami-0a017d8ceb274537d"
-  instance_type = "t3.micro"
-  subnet_id = aws_subnet.public_subnets.*.id[0]
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  tags = {
-    Name = "public"
-  }
-}
-resource "aws_instance" "app" {
-  ami = "ami-0a017d8ceb274537d"
-  instance_type = "t3.micro"
-  subnet_id = aws_subnet.app_subnets.*.id[0]
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  tags = {
-    Name = "app"
-  }
-}
+#resource "aws_instance" "public" {
+#  ami = "ami-0a017d8ceb274537d"
+#  instance_type = "t3.micro"
+#  subnet_id = aws_subnet.public_subnets.*.id[0]
+#  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+#  tags = {
+#    Name = "public"
+#  }
+#}
+#resource "aws_instance" "app" {
+#  ami = "ami-0a017d8ceb274537d"
+#  instance_type = "t3.micro"
+#  subnet_id = aws_subnet.app_subnets.*.id[0]
+#  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+#  tags = {
+#    Name = "app"
+#  }
+#}
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
