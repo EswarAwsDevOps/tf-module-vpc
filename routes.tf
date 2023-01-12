e11147b464af7bf292f49be5bba259c65a3128f6
eswar <eswar.awsdevops@gmail.com>
2023-01-12 15:21:55 +0530

    creating mutable app

--- routes.tf
+++ routes.tf
@@ -78,12 +78,12 @@
 #  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 #}
 
-resource "aws_route" "route_to_app_vpc_in_default_vpc" {
-  route_table_id            = var.default_vpc_route_table
-  destination_cidr_block    = var.vpc_cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
-}
-
+#resource "aws_route" "route_to_app_vpc_in_default_vpc" {
+#  route_table_id            = var.default_vpc_route_table
+#  destination_cidr_block    = var.vpc_cidr_block
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+#}
+#
 
 ### Route tables
 resource "aws_route_table" "public" {
